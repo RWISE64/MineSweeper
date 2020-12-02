f03002819cd422a260558b87f347c88bb495fe1e
rwise <robertwise64@gmail.com>
2020-12-02 15:01:54 -0600

    Coloring for square content

--- src/square/square.js
+++ src/square/square.js
@@ -5,6 +5,8 @@ class Square extends React.Component {
     render() {
         let square = this.props.square;
         let classes = (this.props.shaded) ? ' shaded' : '';
+        classes += (square.mine && !square.hidden) ? ' mine' : '';
+        classes += ' adjMines' + square.adjMineCount;
         let content = (square.hidden) ? null : ((square.mine) ? '*' : square.adjMineCount);
         return (
             <button
